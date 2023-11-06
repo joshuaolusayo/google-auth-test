b0e24dac6c4e2b04a0ad6c12342038f0a6e95767
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2023-11-06 12:01:16 +0100

    frontend app to connect to node backend for google auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, setUser] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // Get the URL parameter value
+        const urlParams = new URLSearchParams(window.location.search);
+        const userParam = urlParams?.get("user");
+
+        if (userParam) {
+            // Decode the URL-encoded JSON string
+            const decodedUserParam = decodeURIComponent(userParam);
+
+            // Parse the JSON string to obtain the desired format
+            const userObject = JSON.parse(decodedUserParam);
+            setUser(userObject);
+            console.log(userObject);
+        }
+
+        setLoading(false);
+    }, []);
+
+    useEffect(() => {}, [user]);
+
+    console.log({ user });
+
+    return (
+        <div className="container">
+            {isLoading ? ( // Display a loading indicator while fetching data
+                <div>Loading...</div>
+            ) : (
+                <Routes>
+                    <Route exact path="/" element={user ? <Home user={user} /> : <Navigate to="/login" />} />
+                    <Route exact path="/login" element={user ? <Navigate to="/" /> : <Login />} />
+                    <Route path="/signup" element={user ? <Navigate to="/" /> : <Signup />} />
+                    <Route path="*" element={user ? <Navigate to="/" /> : <Navigate to="/login" />} />
+                </Routes>
+            )}
+        </div>
+    );
 }
 
 export default App;
